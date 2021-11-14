883c83ea667207fa985392f3790e7323cecbf337
kumasento <kumasento@users.noreply.github.com>
2021-11-14 19:28:20 +0000

    Conv2d example

--- lib/Transforms/MapToDialects.cc
+++ lib/Transforms/MapToDialects.cc
@@ -36,36 +36,10 @@ using namespace pgex;
 
 using PointerToTensor = llvm::DenseMap<mlir::Value, mlir::Value>;
 
-static void createTensorLoad(CallOp caller, PointerToTensor &ptr2tsr,
-                             OpBuilder &b) {
-  OpBuilder::InsertionGuard g(b);
-  Location loc = caller.getLoc();
-
-  // -- step 1: find the tensor object (the llvm.ptr<ptr<i8>> value)
-  assert(caller.getNumResults() == 1);
-
-  auto tv = caller.getResult(0);
-  assert(tv.hasOneUse()); // should be a llvm.store
-
-  auto store = dyn_cast<LLVM::StoreOp>(*tv.user_begin());
-  assert(store);
-
-  auto ptr = store.addr();
-  assert(ptr.getDefiningOp<LLVM::AllocaOp>());
-
-  // -- step 2: build the tensor mapping.
-  b.setInsertionPointAfter(caller);
-  auto tsr = b.create<memref::TensorLoadOp>(loc, caller.getOperand(0));
-  ptr2tsr.insert({ptr, tsr});
-}
-
-static void createTensorStore(CallOp caller, const PointerToTensor &ptr2tsr,
-                              OpBuilder &b) {
-  OpBuilder::InsertionGuard g(b);
-  Location loc = caller.getLoc();
-
-  assert(caller.getNumOperands() == 2);
-  auto tv = caller.getOperand(0);
+/// Get the source tensor register.
+static mlir::Value getLoadedTensor(CallOp caller, unsigned index,
+                                   const PointerToTensor &ptr2tsr) {
+  auto tv = caller.getOperand(index);
 
   auto load = tv.getDefiningOp<LLVM::LoadOp>();
   assert(load);
@@ -74,7 +48,39 @@ static void createTensorStore(CallOp caller, const PointerToTensor &ptr2tsr,
   assert(ptr);
   assert(ptr2tsr.count(ptr));
 
-  auto tsr = ptr2tsr.lookup(ptr);
+  return ptr2tsr.lookup(ptr);
+}
+
+static void storeTensor(CallOp caller, unsigned index, mlir::Value tsr,
+                        PointerToTensor &ptr2tsr) {
+  auto tv = caller.getResult(index);
+  // This is ensured by -simplify-dataflow
+  assert(tv.hasOneUse()); // should be a llvm.store
+  auto store = dyn_cast<LLVM::StoreOp>(*tv.user_begin());
+  assert(store);
+  auto ptr = store.addr();
+  assert(ptr.getDefiningOp<LLVM::AllocaOp>());
+  ptr2tsr.insert({ptr, tsr});
+}
+
+static void createTensorLoad(CallOp caller, PointerToTensor &ptr2tsr,
+                             OpBuilder &b) {
+  OpBuilder::InsertionGuard g(b);
+  Location loc = caller.getLoc();
+
+  b.setInsertionPointAfter(caller);
+  auto tsr = b.create<memref::TensorLoadOp>(loc, caller.getOperand(0));
+
+  storeTensor(caller, 0, tsr, ptr2tsr);
+}
+
+static void createTensorStore(CallOp caller, const PointerToTensor &ptr2tsr,
+                              OpBuilder &b) {
+  OpBuilder::InsertionGuard g(b);
+  Location loc = caller.getLoc();
+
+  assert(caller.getNumOperands() == 2);
+  auto tsr = getLoadedTensor(caller, 0, ptr2tsr);
 
   b.setInsertionPointAfter(caller);
   b.create<memref::TensorStoreOp>(loc, tsr, caller.getOperand(1));
@@ -86,37 +92,77 @@ static void createTOSATensorAdd(CallOp caller, PointerToTensor &ptr2tsr,
   Location loc = caller.getLoc();
 
   assert(caller.getNumOperands() == 2);
-
-  // Mapping operands
-  auto lhs = caller.getOperand(0);
-  auto rhs = caller.getOperand(1);
-
-  auto loadLHS = lhs.getDefiningOp<LLVM::LoadOp>();
-  auto loadRHS = rhs.getDefiningOp<LLVM::LoadOp>();
-  assert(loadLHS && loadRHS);
-
-  auto ptrLHS = loadLHS.addr();
-  auto ptrRHS = loadRHS.addr();
-  assert(ptr2tsr.count(ptrLHS) && ptr2tsr.count(ptrRHS));
-
-  auto tsrLHS = ptr2tsr.lookup(ptrLHS);
-  auto tsrRHS = ptr2tsr.lookup(ptrRHS);
+  auto tsrLHS = getLoadedTensor(caller, 0, ptr2tsr);
+  auto tsrRHS = getLoadedTensor(caller, 1, ptr2tsr);
 
   b.setInsertionPointAfter(caller);
   auto tsr = b.create<tosa::AddOp>(loc, tsrLHS.getType(), tsrLHS, tsrRHS);
 
-  // Mapping result
-  assert(caller.getNumResults() == 1);
+  storeTensor(caller, 0, tsr, ptr2tsr);
+}
 
-  auto tv = caller.getResult(0);
-  assert(tv.hasOneUse()); // should be a llvm.store
+static void getIntegerArray(CallOp caller, SmallVectorImpl<int64_t> &arr,
+                            unsigned begin, unsigned size) {
+  for (unsigned i = begin; i < begin + size; ++i) {
+    auto operand = caller.getOperand(i);
+    auto cst = operand.getDefiningOp<arith::ConstantIntOp>();
+    assert(cst);
+    arr.push_back(cst.value());
+  }
+}
 
-  auto store = dyn_cast<LLVM::StoreOp>(*tv.user_begin());
-  assert(store);
+static mlir::ArrayAttr getArrayAttr(ArrayRef<int64_t> arr, OpBuilder &b) {
+  SmallVector<mlir::Attribute> attrs;
+  transform(arr, std::back_inserter(attrs), [&](int64_t i) {
+    return b.getIntegerAttr(b.getIntegerType(64), i);
+  });
+  return b.getArrayAttr(attrs);
+}
 
-  auto ptr = store.addr();
-  assert(ptr.getDefiningOp<LLVM::AllocaOp>());
-  ptr2tsr.insert({ptr, tsr});
+static void createTOSATensorReshape(CallOp caller, PointerToTensor &ptr2tsr,
+                                    OpBuilder &b) {
+  OpBuilder::InsertionGuard g(b);
+  Location loc = caller.getLoc();
+
+  // Resolve input tensor pointer
+  assert(caller.getNumOperands() >= 2);
+  auto tsr = getLoadedTensor(caller, 0, ptr2tsr);
+
+  // Resolve attributes
+  SmallVector<int64_t> shape;
+  getIntegerArray(caller, shape, 1, caller.getNumOperands() - 1);
+
+  auto ty = tsr.getType().cast<TensorType>();
+  auto newTy = ty.clone(llvm::makeArrayRef(shape));
+
+  b.setInsertionPointAfter(caller);
+  auto reshape =
+      b.create<tosa::ReshapeOp>(loc, newTy, tsr, getArrayAttr(shape, b));
+  storeTensor(caller, 0, reshape.getResult(), ptr2tsr);
+}
+
+static void createTOSAConv2d(CallOp caller, PointerToTensor &ptr2tsr,
+                             OpBuilder &b) {
+  OpBuilder::InsertionGuard g(b);
+  Location loc = caller.getLoc();
+
+  auto input = getLoadedTensor(caller, 0, ptr2tsr);
+  auto weight = getLoadedTensor(caller, 1, ptr2tsr);
+  auto bias = getLoadedTensor(caller, 2, ptr2tsr);
+
+  SmallVector<int64_t> pad, stride, dilation;
+  getIntegerArray(caller, pad, 3, 4);
+  getIntegerArray(caller, stride, 7, 2);
+  getIntegerArray(caller, dilation, 9, 2);
+
+  auto ty = input.getType().cast<TensorType>();
+  auto retTy = UnrankedTensorType::get(ty.getElementType());
+
+  b.setInsertionPointAfter(caller);
+  auto conv2d = b.create<tosa::Conv2DOp>(
+      loc, retTy, input, weight, bias, getArrayAttr(pad, b),
+      getArrayAttr(stride, b), getArrayAttr(dilation, b));
+  storeTensor(caller, 0, conv2d.getResult(), ptr2tsr);
 }
 
 static void cleanUp(ModuleOp m, ArrayRef<Operation *> toErase,
@@ -163,16 +209,18 @@ struct MapToDialectsPass : public ::pgex::MapToDialectsBase<MapToDialectsPass> {
     // Create memref.tensor_load and tensor_store.
     m.walk([&](CallOp caller) {
       auto name = caller.getCallee();
-      if (name.find("tensor_load") != StringRef::npos) {
+      if (name.find("tensor_load") != StringRef::npos)
         createTensorLoad(caller, ptr2tsr, b);
-        toErase.push_back(caller);
-      } else if (name.find("tensor_store") != StringRef::npos) {
+      else if (name.find("tensor_store") != StringRef::npos)
         createTensorStore(caller, ptr2tsr, b);
-        toErase.push_back(caller); // store can be directly removed.
-      } else if (name == "tensor_add") {
+      else if (name == "tensor_add")
         createTOSATensorAdd(caller, ptr2tsr, b);
-        toErase.push_back(caller);
-      }
+      else if (name.find("tensor_reshape") != StringRef::npos)
+        createTOSATensorReshape(caller, ptr2tsr, b);
+      else if (name == "tensor_conv2d")
+        createTOSAConv2d(caller, ptr2tsr, b);
+
+      toErase.push_back(caller);
     });
 
     // Clean up
--- test/MapToDialects/tensor_conv.cc
+++ test/MapToDialects/tensor_conv.cc
@@ -1,4 +1,4 @@
-// RUN: exit 0
+// RUN: mlir-clang %std %s | runner -simplify-dataflow -map-to-dialects | FileCheck %s
 #include <cstdint>
 
 #define Tensor void *
@@ -11,8 +11,8 @@ extern "C" {
 // dilation1)
 Tensor tensor_conv2d(Tensor, Tensor, Tensor, int64_t, int64_t, int64_t, int64_t,
                      int64_t, int64_t, int64_t, int64_t); // -> tosa.conv2d
-Tensor tensor_set_shape1d(Tensor, int64_t);
-Tensor tensor_set_shape4d(Tensor, int64_t, int64_t, int64_t, int64_t);
+Tensor tensor_reshape1d(Tensor, int64_t);
+Tensor tensor_reshape4d(Tensor, int64_t, int64_t, int64_t, int64_t);
 }
 
 int main() {
@@ -22,11 +22,32 @@ int main() {
   Tensor W = tensor_load(weight);
   Tensor b = tensor_load(bias);
 
-  X = tensor_set_shape4d(X, 2, 3, 32, 32);
-  W = tensor_set_shape4d(W, 4, 3, 3, 3);
-  b = tensor_set_shape1d(b, 4);
+  X = tensor_reshape4d(X, 2, 3, 32, 32);
+  W = tensor_reshape4d(W, 4, 3, 3, 3);
+  b = tensor_reshape1d(b, 4);
 
-  Tensor Y = tensor_conv2d(tensor_set_shape4d(X, 2, 3, 32, 32), W, b, 1, 1, 1,
-                           1, 1, 1, 1, 1);
+  Tensor Y = tensor_conv2d(X, W, b, 1, 1, 1, 1, 1, 1, 1, 1);
+  Y = tensor_reshape1d(Y, -1);
   tensor_store(Y, output);
+  return 0;
 }
+
+// CHECK: func @main
+// CHECK: %[[v0:.*]] = memref.alloca() : memref<8192xf32>
+// CHECK: %[[v1:.*]] = memref.alloca() : memref<4xf32>
+// CHECK: %[[v2:.*]] = memref.alloca() : memref<108xf32>
+// CHECK: %[[v3:.*]] = memref.alloca() : memref<6144xf32>
+// CHECK: %[[v4:.*]] = memref.cast %[[v3]] : memref<6144xf32> to memref<?xf32>
+// CHECK: %[[v5:.*]] = memref.tensor_load %[[v4]] : memref<?xf32>
+// CHECK: %[[v6:.*]] = memref.cast %[[v2]] : memref<108xf32> to memref<?xf32>
+// CHECK: %[[v7:.*]] = memref.tensor_load %[[v6]] : memref<?xf32>
+// CHECK: %[[v8:.*]] = memref.cast %[[v1]] : memref<4xf32> to memref<?xf32>
+// CHECK: %[[v9:.*]] = memref.tensor_load %[[v8]] : memref<?xf32>
+// CHECK: %[[v10:.*]] = "tosa.reshape"(%[[v5]]) {new_shape = [2, 3, 32, 32]}
+// CHECK: %[[v11:.*]] = "tosa.reshape"(%[[v7]]) {new_shape = [4, 3, 3, 3]}
+// CHECK: %[[v12:.*]] = "tosa.reshape"(%[[v9]]) {new_shape = [4]}
+// CHECK: %[[v13:.*]] = "tosa.conv2d"(%[[v10]], %[[v11]], %[[v12]])
+// CHECK: {dilation = [1, 1], pad = [1, 1, 1, 1], stride = [1, 1]}
+// CHECK: %[[v14:.*]] = "tosa.reshape"(%[[v13]]) {new_shape = [-1]}
+// CHECK: %[[v15:.*]] = memref.cast %[[v0]] : memref<8192xf32> to memref<?xf32>
+// CHECK: memref.tensor_store %[[v14]], %[[v15]] : memref<?xf32>
